c7f7f1f186682a493a1bfbdd21e7e7cdde864500
bwhitmore7 <bryson.whitmore@patelhs.org>
2020-11-03 23:31:30 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+class Main {
+  public static void main(String[] args) {
+    String[] dogs = {"beagle", "bulldog", "dachshund", "italian greyhound", "pug", "shiba inu", "siberian husky", "german shorthaired pointer", "chocolate Retriever", "dalmatian"};
+    for (int i = 0; i < 9; i++) {
+    System.out.println(dogs[i]);
+    }
+
+    dogs[7] = "siberian husky";
+
+    System.out.println(" \n here is my updated dog array \n"); 
+
+    for(int i = 1; i < 10; i++) {
+      System.out.println(dogs[6]); 
+    }
+
+    String[] i = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+    for (int )
+
+  }
+}
