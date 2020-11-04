c20dc34db8d6859f61017b7aed5ec6d016b90ff0
bwhitmore7 <bryson.whitmore@patelhs.org>
2020-11-04 22:39:46 +0000

    i changed

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-class Main {
+public class Main {
   public static void main(String[] args) {
     String[] dogs = {"beagle", "bulldog", "dachshund", "italian greyhound", "pug", "shiba inu", "siberian husky", "german shorthaired pointer", "chocolate Retriever", "dalmatian"};
     for (int i = 0; i < 9; i++) {
@@ -12,12 +12,11 @@ class Main {
     for(int i = 1; i < 10; i++) {
       System.out.println(dogs[6]); 
     }
-
-    String[] i = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
-    for (int i = 3; i > 0; i--){
-    if (int[] % 3==0)
+String[] i = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+    for (int i = 4; b > 0; i--){
+    if (int [i] % 4==0) 
     System.out.println(dogs[i]);
     }
-
+    // food[3] % 2 == 0
   }
 }
