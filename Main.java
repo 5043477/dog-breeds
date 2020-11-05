42eab3bdba1c20c4dcf41f493a4386baff894c96
bwhitmore7 <bryson.whitmore@patelhs.org>
2020-11-05 14:04:40 +0000

    everythng

--- Main.java
+++ Main.java
@@ -1,22 +1,18 @@
 public class Main {
   public static void main(String[] args) {
-    String[] dogs = {"beagle", "bulldog", "dachshund", "italian greyhound", "pug", "shiba inu", "siberian husky", "german shorthaired pointer", "chocolate Retriever", "dalmatian"};
+    String[] dogs = { "beagle", "bulldog", "dachshund", "italian greyhound", "pug", "shiba inu", "siberian husky",
+        "german shorthaired pointer", "chocolate Retriever", "dalmatian" };
+
     for (int i = 0; i < 9; i++) {
-    System.out.println(dogs[i]);
+      System.out.println(dogs[i]);
     }
 
     dogs[7] = "siberian husky";
 
-    System.out.println(" \n here is my updated dog array \n"); 
+    System.out.println(" \n here is my updated dog array \n");
 
-    for(int i = 1; i < 10; i++) {
-      System.out.println(dogs[6]); 
+    for (int i = 1; i < 10; i++) {
+      System.out.println(dogs[6]);
     }
-String[] i = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
-    for (int i = 4; b > 0; i--){
-    if (int [i] % 4==0) 
-    System.out.println(dogs[i]);
-    }
-    // food[3] % 2 == 0
   }
 }
